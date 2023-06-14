512007eeb9330edd05cb23cd0c7153362341b08b
Adithya Narayan <67658457+adithyang64@users.noreply.github.com>
2023-06-14 22:50:52 +0530

    Update kafka_spark_producer_timeprovince.py

--- kafka_spark_producer_timeprovince.py
+++ kafka_spark_producer_timeprovince.py
@@ -14,14 +14,14 @@ FILE_PATH = "/Users/Adithya/Desktop/iNeuron/Spark/Assignment/TimeProvince.csv"
 columns = ['date', 'time', 'province', 'confirmed', 'released', 'deceased']
 
 
-API_KEY = 'GHVGAQVJ3RSWJEKA'
+API_KEY = ''
 ENDPOINT_SCHEMA_URL  = 'https://psrc-znpo0.ap-southeast-2.aws.confluent.cloud'
-API_SECRET_KEY = 'g1rJY1gedecr723jMlYJnNEOtAVinFdcBnq+lsqpAVhJ5GWU3lXxrDP1gztETSmM'
+API_SECRET_KEY = ''
 BOOTSTRAP_SERVER = 'pkc-l7pr2.ap-south-1.aws.confluent.cloud:9092'
 SECURITY_PROTOCOL = 'SASL_SSL'
 SSL_MECHANISM = 'PLAIN'
-SCHEMA_REGISTRY_API_KEY = 'GHV5DZXEKMAQQGGQ'
-SCHEMA_REGISTRY_API_SECRET = '91zPqTVUYyvF0wqjR7p4Z4np4k8x7nWGMVJhYPhqhkcttiUrI45I5YMbnW0r4NbQ'
+SCHEMA_REGISTRY_API_KEY = ''
+SCHEMA_REGISTRY_API_SECRET = ''
 
 
 def sasl_conf():
